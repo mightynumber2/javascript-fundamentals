b136e369ed988a42ac10607f81911a9826ad01c8
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-09 08:45:03 -0700

    Complete declare variables assignment

--- assignments.js
+++ assignments.js
@@ -1,15 +1,14 @@
+/*
 // JAVASCRIPT FUNDAMENTALS - PART 1
 
 // Lecture: Values and Variables
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
-
 let country = "United States";
 let continent = "North America";
 let population = 332;
 
 // 2. Log their values to the console
-
 console.log(country);
 console.log(continent);
 console.log(population);
@@ -18,14 +17,27 @@ console.log(population);
 // 1. Declare a variable called 'isIsland' and set its value according to your
 // country. The variable should hold a Boolean value. Also declare a variable
 // 'language', but don't assign it any value yet
-
 let isIsland = false;
 let language;
 
 // 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 // to the console
-
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+*/
+
+// Lecture: let, const and var
+// 1. Set the value of 'language' to the language spoken where you live (some
+// countries have multiple languages, but just choose one)
+const language = "English";
+
+// 2. Think about which variables should be const variables (which values will never
+// change, and which might change?). Then, change these variables to const.
+const isIsland = false;
+const country = "United States";
+const continent = "North America";
+
+// 3. Try to change one of the changed variables now, and observe what happens
+// language = "Spanish";
