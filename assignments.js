63953eedcbbc68fa10db7a9b6e865ce012f82d00
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-08 20:41:47 -0700

    Add/complete 01-fundamentals values and variables assignment

---
+++ assignments.js
@@ -0,0 +1,15 @@
+// JAVASCRIPT FUNDAMENTALS - PART 1
+
+// Lecture: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+
+let country = "United States";
+let continent = "North America";
+let population = "332 million";
+
+// 2. Log their values to the console
+
+console.log(country);
+console.log(continent);
+console.log(population);
