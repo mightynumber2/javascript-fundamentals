6b37fafb219042f71238b7f767237db813b6af97
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-08 21:22:58 -0700

    Add data-type examples of dynamic typing

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -9,3 +10,25 @@ let firstName = "Dan";
 console.log(firstName);
 console.log("is");
 console.log(js);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 41);
+console.log(typeof "Dan");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1981;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
