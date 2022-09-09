1bad9e77dec3194d831eabcabbb74a94bb22ec69
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-08 21:40:24 -0700

    Complete data-types assignment

--- assignments.js
+++ assignments.js
@@ -6,10 +6,26 @@
 
 let country = "United States";
 let continent = "North America";
-let population = "332 million";
+let population = 332;
 
 // 2. Log their values to the console
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Lecture: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+
+let isIsland = false;
+let language;
+
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
