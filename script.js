b5b2bae15759a6ac4f12a1d6756ae5abc2b1f056
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-09 08:28:20 -0700

    Add var declartion practice

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let firstName = "Dan";
 console.log(firstName);
 console.log("is");
 console.log(js);
-*/
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -32,3 +31,20 @@ console.log(year);
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+
+// Let - is a dynamic variable that can mutate and is not the best var declaration, as it can "dynamically" introduce bugs--LOL
+let age = 40;
+age = 41;
+
+// Const is a static var--the most reliable and safe way to use var. It cannot mutate.
+const birthYear = 1981;
+
+// Var is the outdated and similar version of let.
+var job = "unemployed";
+job = "web developer";
+
+// You can also assign a var without declaring it! It's assigned globally?, and it's not a good idea.
+
+lastName = "Humperdink";
+console.log(lastName);
