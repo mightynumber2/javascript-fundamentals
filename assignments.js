f002cc303088692635a075326bc583420a4216e4
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-09 21:00:46 -0700

    Complete if/else statements assignment

--- assignments.js
+++ assignments.js
@@ -29,13 +29,13 @@ let population = 332;
 // Lecture: let, const and var
 // 1. Set the value of 'language' to the language spoken where you live (some
 // countries have multiple languages, but just choose one)
-const language = 'English';
+const language = "English";
 
 // 2. Think about which variables should be const variables (which values will never
 // change, and which might change?). Then, change these variables to const.
 const isIsland = false;
-const country = 'The United States';
-const continent = 'North America';
+const country = "The United States";
+const continent = "North America";
 
 // 3. Try to change one of the changed variables now, and observe what happens
 // language = "Spanish";
@@ -75,3 +75,23 @@ population++;
 // using the template literal syntax
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
 // console.log(description);
+
+/*
+LECTURE: Taking Decisions: if / else Statements
+
+1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+    );
+  }
+  */
