b0c55eb774d236b041ead119c811ba0aaf9dcc4b
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-09 19:31:44 -0700

    Complete strings and template literals assignment

--- assignments.js
+++ assignments.js
@@ -29,13 +29,13 @@ let population = 332;
 // Lecture: let, const and var
 // 1. Set the value of 'language' to the language spoken where you live (some
 // countries have multiple languages, but just choose one)
-const language = "English";
+const language = 'English';
 
 // 2. Think about which variables should be const variables (which values will never
 // change, and which might change?). Then, change these variables to const.
 const isIsland = false;
-const country = "The United States";
-const continent = "North America";
+const country = 'The United States';
+const continent = 'North America';
 
 // 3. Try to change one of the changed variables now, and observe what happens
 // language = "Spanish";
@@ -43,29 +43,35 @@ const continent = "North America";
 // LECTURE: Basic Operators
 // 1. If your country split in half, and each half would contain half the population,
 // then how many people would live in each half?
-console.log(population / 2);
+// console.log(population / 2);
 
 // 2. Increase the population of your country by 1 and log the result to the console
 population++;
-console.log(population);
+// console.log(population);
 
 // 3. Finland has a population of 6 million. Does your country have more people than
 // Finland?
-console.log(population > 6);
+// console.log(population > 6);
 
 // 4. The average population of a country is 33 million people. Does your country
 // have less people than the average country?
-console.log(population < 33);
+// console.log(population < 33);
 
 // 5. Based on the variables you created, create a new variable 'description'
 // which contains a string with this format: 'Portugal is in Europe, and its 11 million
 // people speak portuguese'
-const description1 =
-  country +
-  " is in " +
-  continent +
-  ", and it's " +
-  population +
-  " million people speak " +
-  language;
-console.log(description1);
+// const description1 =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and it's " +
+//   population +
+//   " million people speak " +
+//   language;
+// console.log(description1);
+
+// LECTURE: Strings and Template Literals
+// 1. Recreate the 'description' variable from the last assignment, this time
+// using the template literal syntax
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+// console.log(description);
