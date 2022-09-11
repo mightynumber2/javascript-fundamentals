45212004a5395542adf715aea3c59547dbd3ddc7
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-11 11:30:06 -0700

    Complete the switch statement assignment

--- assignments.js
+++ assignments.js
@@ -150,4 +150,32 @@ if (language === "English" && population < 50 && !isIsland) {
 } else {
   console.log(`${country} does not meet your criteria :(`);
 }
+
+
+////////////////////////////////////////////
+// LECTURE: The switch Statement
+
+const language = "english";
+console.log(language);
+
+switch (language) {
+  case 'chinese':
+  case 'mandarin':
+    console.log("MOST number of native speakers!");
+    break;
+  case 'spanish':
+    console.log("2nd place in number of native speakers");
+    break;
+  case 'english':
+    console.log("3rd place");
+    break;
+  case 'hindi':
+    console.log("Number 4");
+    break;
+  case 'arabic':
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D")
+}
 */
