eef468a2b9b20f4e1f46ccf7813b27a54f3a2855
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-12 11:48:04 -0700

    Add functions practice

--- 02-Fundamentals/script.js
+++ 02-Fundamentals/script.js
@@ -1,6 +1,10 @@
+////////////////////////////////////////
+// ACTIVATING STRICT MODE
+
 // Restricts and warns, preventing bugs; Always use
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -10,3 +14,29 @@ if (hasDriversLicense) console.log("I can drive!");
 
 // const interface = "Audio";
 // const private = "Parts";
+
+////////////////////////////////////////
+// FUNCTIONS
+
+// A function is a block of code that can be used repeatedly
+function logger() {
+  console.log("My name is Dan the Man");
+}
+
+// call - run - invoke
+logger();
+logger();
+logger();
+
+function foodProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+// console.log(foodProcessor(5, 0));
+const appleJuice = foodProcessor(5,0);
+console.log(appleJuice);
+const appleOrangeJuice = foodProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
