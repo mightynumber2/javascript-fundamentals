2dd5d2ddf5580f3b994e1714a3f45c50bc75ccc8
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-12 17:38:14 -0700

    Fix assignments link to 02-index and add functions declarations/expressions examples

--- 02-Fundamentals/script.js
+++ 02-Fundamentals/script.js
@@ -39,4 +39,23 @@ const appleJuice = foodProcessor(5,0);
 console.log(appleJuice);
 const appleOrangeJuice = foodProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+////////////////////////////////////////
+// FUNCTIONS DECLARATIONS VS EXPRESSIONS
+
+// Function declaration
+function ageCalc1(birthYear) {
+  return 2022 - birthYear;
+}
+// call function using an argument
+// you can call this function before declaring it!
+const age1 = ageCalc1(1981);
+
+// Function expression
+// have to call the function after creating it
+const ageCalc2 = function (birthYear) {
+  return 2022 - birthYear;
+}
+const age2 = ageCalc2(1981);
+console.log(age1, age2);
 */
