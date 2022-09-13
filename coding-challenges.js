8b123abfb855efc296f29c54133c12e803000717
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-12 22:32:38 -0700

    Link coding-challenges to 02-index and complete 02-Fundamentals challenge #1

--- coding-challenges.js
+++ coding-challenges.js
@@ -124,4 +124,25 @@ const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
 console.log(tip);
 // 2.
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+/////////////////////////////////////////
+// JavaScript Fundamentals – Part 2
+
+//Coding Challenge #1///////////////////
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// Data Tests
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log("No team wins!");
+  }
+}
+checkWinner(scoreDolphins, scoreKoalas);
 */
