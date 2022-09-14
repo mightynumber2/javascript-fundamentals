58949e5743797035a3ccffcf70fdb0cd3de40aa0
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-14 10:41:01 -0700

    Complete 02-fundamentals coding challenge #2

--- coding-challenges.js
+++ coding-challenges.js
@@ -146,3 +146,30 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 }
 checkWinner(scoreDolphins, scoreKoalas);
 */
+
+//Coding Challenge #2///////////////////
+// 1.
+// const calcTip = function (bill) {
+//   if (bill >= 50 && bill <= 300) {
+//     return bill * .15
+//   } else {
+//     return bill * .20
+//   }
+// }
+// ternary conditonal operator version of calcTip function
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+// }
+/*
+// arrow function with ternary operator
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(calcTip(100));
+// 2.
+const bills = [125, 555, 44];
+// 3.
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+console.log(bills, tips);
+// 4.
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total); // 143.75, 666, 52.8
+*/
