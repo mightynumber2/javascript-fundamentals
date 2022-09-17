8224f9e0fae4488c3191154c0a7924624daca040
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-16 18:14:51 -0700

    Complete 02-coding-challenge #4, thanks Google

--- coding-challenges.js
+++ coding-challenges.js
@@ -200,4 +200,32 @@ if (mark.BMI > john.BMI) {
 } else {
   console.log(`${john.fullName}'s BMI(${john.BMI}) is higher than ${mark.fullName}'s (${mark.BMI})!`);
 }
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52
+];
+console.log(bills);
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.20;
+  };
+}
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
+console.log(tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  };
+  return sum / arr.length;
+}
+console.log(calcAverage(totals));
 */
