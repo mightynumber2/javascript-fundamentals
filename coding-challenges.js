0cb4ad4fe69b40c719cef8c67adac52b47871819
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-01 07:12:25 -0700

    Second attempt at 09 coding challenge #1

--- coding-challenges.js
+++ coding-challenges.js
@@ -313,7 +313,8 @@ const game = {
   },
   };
 
-// 1. Create one player array for each team (variables 'players1' and 'players2')
+// Attempt 2
+// 1. Create one player array for each team (variables 'players1' and'players2')
 const [players1, players2] = game.players;
 console.log(players1, players2);
 // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
@@ -325,12 +326,12 @@ console.log(allPlayers);
 // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 console.log(players1Final);
-// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2'
-const {team1, x: draw, team2} = game.odds;
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const {odds: {team1, x: draw, team2}} = game;
 console.log(team1, draw, team2);
 // 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-function printGoals(...playerNames) {
-  console.log(`${playerNames.length} goals were scored`);
+function printGoals(...players) {
+  console.log(`${players.length} goals scored in total`);
 }
 printGoals(...game.scored);
 // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
