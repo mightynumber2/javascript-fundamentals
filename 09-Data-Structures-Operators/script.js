d02b77ac51139b597f52dbe98290e3209d493970
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-02 22:43:06 -0700

    Add ES6-enhanced object literals examples

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -3,6 +3,24 @@
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  
+const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+const openingHours = {
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    [weekdays[4]]: {
+      open: 11,
+      close: 23,
+    },
+    // ES6: property names can be computed
+    [weekend[5]]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  };
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,20 +30,8 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enhancement allows us to easily insert external objects
+  openingHours,
 
   order: function(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -35,11 +41,12 @@ const restaurant = {
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} at ${time} hours for ${address}`);
   },
   
-  orderPasta: function(ing1, ing2, ing3) {
+  // ES6 way of handling inner-object functions
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Pasta coming up! using ${ing1}, ${ing2}, and ${ing3}!`)
   },
   
-  orderPizza: function(mainIng, ...otherIngs) {
+  orderPizza(mainIng, ...otherIngs) {
     mainIng = mainIng.charAt(0).toUpperCase() + mainIng.slice(1);
     if (otherIngs == 0) {
       console.log(`${mainIng} pizza ordered!`);
@@ -49,7 +56,7 @@ const restaurant = {
   }
 };
 
-///////////////////////////////////////////////////// 
+/* ///////////////////////////////////////////////////// 
 // THE FOR-OF LOOP
 
 // Grab all menu items through destructuring using the spread operator
@@ -63,7 +70,7 @@ console.log(`${item[0] + 1} ${item[1]}`);
 console.log([...menu.entries()]);
 // New way by destructuring item
 for (const [i, value] of menu.entries())
-console.log(`${i + 1}: ${value}`);
+console.log(`${i + 1}: ${value}`); */
 
 /* // OR assignment operator
 const restUno = {
