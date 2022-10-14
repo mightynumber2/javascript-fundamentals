697b3b52d03ba0bbaa047c524013fb93f5c4e4ac
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-14 15:00:33 -0700

    Add [10] default parameters examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers = 1, price = 150 * numPassengers) {  
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  bookings[0] = booking;
+};
+
+createBooking('AZ519', undefined,);
+console.log(bookings);
