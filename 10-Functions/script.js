75bea89fdad5f8a5fad8d215917b9386123bfcbf
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-15 07:09:51 -0700

    Practice [10] higher-order and functions-return-functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const bookings = [];
+///////////////////////////////////////////
+// MORE FUNCTIONS
+
+// Default parameters
+/* const bookings = [];
 
 const createBooking = function(flightNum, numPassengers = 1, price = 150 * numPassengers) {  
   const booking = {
@@ -13,4 +17,43 @@ const createBooking = function(flightNum, numPassengers = 1, price = 150 * numPa
 };
 
 createBooking('AZ519', undefined,);
-console.log(bookings);
+console.log(bookings); */
+
+///////////////////////////////////////////
+// HIGHER-ORDER FUNCTIONS
+
+// First-class/call-back functions
+/* const oneWord = function(str) {
+  return str.replace(/ /g, '').toLowerCase();
+}
+console.log(oneWord('JavaScript is the best!'));
+
+const firstUpperCase = str => {
+  const [first, ...rest] = str.split(' ');
+  return [first.toUpperCase(), ...rest].join(' ');
+};
+console.log(firstUpperCase('JavaScript is the best!'));
+
+// Higher-order function
+const transform = function(str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+}
+transform('JavaScript is the best!', firstUpperCase);
+
+// Callback using the DOM
+const high5 = function() {
+  console.log('✋');
+};
+
+///////////////////////////////////////////
+// FUNCTIONS RETURNING FUNCTIONS
+
+document.body.addEventListener('click', high5);
+['Dan', 'Jonas', 'Leon', 'Colt'].forEach(high5); */
+
+const greet = greeting => name =>
+  console.log(`${greeting} ${name}`);
+
+greet('Hello')('Dan');
