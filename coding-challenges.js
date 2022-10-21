b8bd3a91f6f8793bf1933ad95511e36ddcf38817
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-21 07:31:08 -0700

    Complete [11] coding challenge #3

--- coding-challenges.js
+++ coding-challenges.js
@@ -621,3 +621,13 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]); */
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]); */
+
+//Coding Challenge #3///////////////////
+
+/* const calcAverageHumanAge = ages => ages
+  .map(age => age <= 2 ? 2 * age : 16 + age * 4)
+  .filter(age => age >= 18)
+  .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])); */
