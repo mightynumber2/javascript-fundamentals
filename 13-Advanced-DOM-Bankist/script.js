a6d82abc4d7179ba223344c77928b05fd3eada09
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-21 22:57:48 -0700

    [13] Add creating, selecting, and deleting dom elements practice

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -33,3 +33,41 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Select parts of the document////////////////////////////
+/* console.log(document.documentElement) // entire document, html
+console.log(document.head)
+console.log(document.body) */
+// Selects all as NODES; more modern way to select
+const header = document.querySelector('.header')
+const allSections = document.querySelectorAll('.section')
+// Selects all as LIVE HTMLCollections, which update in real time unlike NODES
+const allButtons = document.getElementsByTagName('button')
+// console.log(allButtons)
+// console.log(document.getElementsByClassName('btn'))
+
+// Creating and inserting elements/////////////////////////
+// .insertAdjacentHTML
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+// message.textContent = 'We use cookies to annoy the crud out of you and force you to click off this box'
+message.innerHTML = 'We use cookies to annoy the crud out of you and force you to click off this box. <button class="btn btn--close--cookie">Got it!</button>'
+
+// Adds as the first child
+// header.prepend(message)
+// last child
+header.append(message)
+// clone to have two of the same message
+// header.append(message.cloneNode(true))
+
+// Adds as siblings, rather than children
+// header.before(message)
+// header.after(message)
+
+// Delete elements
+document.querySelector('.btn--close--cookie').addEventListener('click', function() {
+  message.remove()
+  // message.parentElement.removeChild(message) // old way via dom traversing
+})
