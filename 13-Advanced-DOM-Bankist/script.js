1bfbcf51eda2eb6973afa3d896a9a044f8b0f92f
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-23 01:43:30 -0700

    [13] Add styles, attributes, and classes notes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -71,3 +71,48 @@ document.querySelector('.btn--close--cookie').addEventListener('click', function
   message.remove()
   // message.parentElement.removeChild(message) // old way via dom traversing
 })
+
+// Inline styles
+message.style.background = '#37383d'
+message.style.width = '120%'
+// cannot access styles this way
+/* console.log(message.style.color)
+console.log(message.style.backgroundColor) */
+// rather this way
+/* console.log(getComputedStyle(message).color)
+console.log(getComputedStyle(message).height) */
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 40 + 'px'
+
+// change the root variable color using setProperty() on the document
+document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+// Get standard attributes
+const logo = document.querySelector('.nav__logo')
+console.log(logo.alt)
+console.log(logo.src)
+console.log(logo.className)
+// assign an attribute
+logo.alt = 'Beautiful minimalist logo'
+console.log(logo.alt)
+// set attributes
+logo.setAttribute('company', 'Bankist')
+// get non-standard attributes with:
+console.log(logo.getAttribute('company'))
+
+// Shows absolute url
+console.log(logo.src)
+// compared to relative url
+console.log(logo.getAttribute('src'))
+
+// Data attributes
+logo.setAttribute('data-version-number', '3.0')
+console.log(logo.dataset.versionNumber)
+
+// Classes, can do multiple
+logo.classList.add('c', 'd')
+logo.classList.remove('c', 'd')
+logo.classList.toggle()
+logo.classList.contains() // not includes!
+// do not use; it overrides existing classes
+logo.className = 'b';
